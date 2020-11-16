5ae976d43c41638ddc3c112243e914b799dbf590
Paul Johnson <pcdjohnson@gmail.com>
2020-11-16 21:40:15 +0000

    minor edits

--- CMP-Example.R
+++ CMP-Example.R
@@ -1,7 +1,34 @@
+
+# good talk by Galit Shmuéli on the CMP:
+# https://youtu.be/Nt7gmKmxjxA
+library(COMPoissonReg)
 library(degreenet)
+library(compoisson)
 library(glmmTMB)
-simcmp(n=100, v=c(0.5,sqrt(0.5)), maxdeg=10000)
+y <- simcmp(n=100, v=c(1,sqrt(5)), maxdeg=10000) - 1
+barplot(table(y))
+mean(y)
+var(y)
 
-?glmmTMB
 
-??rcompois
+fit <- glmmTMB(y ~ 1, family = "compois")
+summary(fit)
+exp(fixef(fit)$cond)
+
+sigma(fit)
+
+names(fit)
+fit$fit
+
+fit2 <- glm.cmp(y ~ 1)
+coef(fit2)
+predict(fit2)
+nu(fit2)[1]
+
+
+mean(y^nu(fit2))
+
+simulate(fit)[[1]]
+
+
+
